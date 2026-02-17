c468738bb2056fda0c82d5c4953373a595cbbc25
Davidazukatech <camillacabelloofficalfpg@gmail.com>
2026-02-17 10:56:49 +0100

    Add files via upload

---
+++ slider.js
@@ -0,0 +1,20 @@
+let slides = document.querySelectorAll(".slide");
+let index = 0;
+
+setInterval(() => {
+  slides[index].classList.remove("active");
+  index = (index + 1) % slides.length;
+  slides[index].classList.add("active");
+}, 4000);
+
+// Show popup after 2 seconds
+window.onload = function () {
+  setTimeout(() => {
+    document.getElementById("servicePopup").style.display = "flex";
+  }, 2000);
+};
+
+// Close popup
+function closePopup() {
+  document.getElementById("servicePopup").style.display = "none";
+}
